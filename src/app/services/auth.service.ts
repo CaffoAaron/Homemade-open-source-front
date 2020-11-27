3217d55a3212c8cc5e18f858eaaf491acdf8ea6b
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2020-11-27 13:06:52 -0500

    registrate

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 const AUTH_API = 'https://homemade20201124161107.azurewebsites.net/api/user/';
+const AUTH_API2 = 'https://homemade20201124161107.azurewebsites.net/api/';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -29,5 +30,18 @@ export class AuthService {
       email: user.email,
       password: user.password
     }, httpOptions);
+  }
+  postUser(): Observable<any>{
+    return this.http.post(AUTH_API2 + 'userchef',
+      {
+        name: 'aaron',
+        lastname: 'string',
+        password: 'string',
+        email: 'string',
+        picture: 'string',
+        date: '2020-11-27T17:54:31.570Z',
+        gender: true,
+        certificate: 'string'
+      });
   }
 }
